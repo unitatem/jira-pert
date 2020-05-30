c0b1877aff45928fd039b40d0e2b5b5545d244cc
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2020-05-30 16:39:24 +0200

    Find issues in epic

--- jira-pert/app.py
+++ jira-pert/app.py
@@ -2,15 +2,20 @@ from jira import JIRA
 from .server_connection import server_connection
 
 
-def main():
-    print('jira-pert is running...\n')
-
+def setup_connection():
     options = {'server': server_connection['server_https']}
     jira = JIRA(options=options,
                 basic_auth=(server_connection['email'], server_connection['api_token']))
+    return jira
 
-    projects = jira.projects()
-    print(projects)
+
+def main():
+    print('jira-pert is running...\n')
+    jira = setup_connection()
+
+    epic_key = 'MAR-36'
+    features = jira.search_issues("'Epic Link' = {epic_key}".format(epic_key=epic_key))
+    print(features)
 
 
 if __name__ == "__main__":
