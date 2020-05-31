d9444f1b57e95fdeea28925231974b2122587c01
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2020-05-31 16:47:43 +0200

    Modified node shape

--- jira-pert/pert_diagram.py
+++ jira-pert/pert_diagram.py
@@ -8,13 +8,32 @@ class PertDiagram(object):
         self._model = graph
 
     def plot(self):
-        self._model.print()
-
         graph = nx.DiGraph()
+
         for key, node in self._model.get_graph().items():
             graph.add_node(key)
             for dependency in node.dependencies:
                 graph.add_edge(dependency, key)
 
-        nx.draw(graph, with_labels=True)
+        self._draw(graph)
+
+    def _draw(self, graph):
+        # nx.draw_networkx()
+        # nx.draw_networkx_edges()
+        nx.draw(graph,
+                pos=nx.spectral_layout(graph),
+                with_labels=True,
+                **self._get_node_style())
         plt.show()
+
+    @staticmethod
+    def _get_node_style():
+        return dict(
+            node_size=2200,
+            node_color='blue',
+            edgecolors='blue',
+            bbox=dict(
+                facecolor='white',
+                edgecolor='blue',
+                boxstyle='round,pad=0.5')
+        )
