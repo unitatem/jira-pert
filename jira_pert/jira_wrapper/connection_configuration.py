4da87c44bcb739977955a8f71c997b3eeec66c68
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2020-11-20 19:54:57 +0100

    Handle missing connection configration file

--- jira_pert/jira_wrapper/connection_configuration.py
+++ jira_pert/jira_wrapper/connection_configuration.py
@@ -3,13 +3,20 @@ import json
 
 class ConnectionConfiguration(object):
     def __init__(self):
-        data = self._load_config_file()
-        self.server_https = data['server_https']
-        self.email = data['email']
-        self.api_token = data['api_token']
+        try:
+            self._load_config_file()
+        except FileNotFoundError:
+            print("W: File not found: server_connection.json")
+            self._get_config_data()
 
-    @staticmethod
-    def _load_config_file():
+    def _load_config_file(self):
         with open('jira_pert/jira_wrapper/server_connection.json') as file:
             data = json.load(file)
-        return data
+            self.server_https = data['server_https']
+            self.email = data['email']
+            self.api_token = data['api_token']
+
+    def _get_config_data(self):
+        self.server_https = input("Server https address:")
+        self.email = input("User email address:")
+        self.api_token = input("Authentication key:")
